314a4c841fb395969c20b67ae5f053da33cf2944
Vitalii Berko <vitalii.v.berko@gmail.com>
2023-10-20 15:37:08 +0300

    Begin add screens and styles

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world! Hello world! </Text>
+      <Text style={styles.title}>Hello world! Hello world! </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +17,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    marginTop: 16,
+    paddingVertical: 8,
+    borderWidth: 4,
+    borderColor: '#20232a',
+    borderRadius: 6,
+    backgroundColor: '#61dafb',
+    color: '#20232a',
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+  }
 });
