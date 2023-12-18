46d06dd52f5fc02c90ddc5e5af3fd18ea0bf6e7e
Vitalii Berko <vitalii.v.berko@gmail.com>
2023-12-18 15:47:48 +0200

    Navigation

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
-import { StyleSheet} from "react-native";
+import "react-native-gesture-handler";
+import { StyleSheet } from "react-native";
 import { useFonts } from "expo-font";
-
 import React from "react";
 import RegistrationScreen from "./Screens/auth/RegistrationScreen";
 import LoginScreen from "./Screens/auth/LoginScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import {Provider} from "react-redux"
+import { Main } from "./components/Main";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -17,28 +20,28 @@ export default function App() {
   }
 
   return (
-
-      <LoginScreen/>
- 
+    <Provider store={store}>
+      <Main/>
+    </Provider>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#eaeaea",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    marginTop: 16,
-    paddingVertical: 8,
-    borderWidth: 4,
-    borderColor: "#20232a",
-    borderRadius: 6,
-    backgroundColor: "#61dafb",
-    color: "#20232a",
-    textAlign: "center",
-    fontSize: 30,
-    fontWeight: "bold",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#eaeaea",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   title: {
+//     marginTop: 16,
+//     paddingVertical: 8,
+//     borderWidth: 4,
+//     borderColor: "#20232a",
+//     borderRadius: 6,
+//     backgroundColor: "#61dafb",
+//     color: "#20232a",
+//     textAlign: "center",
+//     fontSize: 30,
+//     fontWeight: "bold",
+//   },
+// });
---
+++ components/Main.js
@@ -0,0 +1,15 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+export const Main = () => {
+  const isLoggedIn = useSelector();
+  const dispatch = useDispatch();
+
+  useEffect(()=>{dispatch()}, [])
+  return (
+    <NavigationContainer>
+      <Routes isAuth={isLoggedIn} />
+    </NavigationContainer>
+  );
+};
