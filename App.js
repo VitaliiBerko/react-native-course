907e33143402a3a7b32a806dd7e1b36768d643ef
Vitalii Berko <vitalii.v.berko@gmail.com>
2024-01-16 16:41:22 +0200

    Created redux, firebase

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import LoginScreen from "./Screens/auth/LoginScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import {Provider} from "react-redux"
 import { Main } from "./components/Main";
+import store from "./redux/store";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -20,28 +21,9 @@ export default function App() {
   }
 
   return (
-    // <Provider store={store}>
+    <Provider store={store}>
       <Main/>
-    // </Provider>
+    </Provider>
   );
 }
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#eaeaea",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-//   title: {
-//     marginTop: 16,
-//     paddingVertical: 8,
-//     borderWidth: 4,
-//     borderColor: "#20232a",
-//     borderRadius: 6,
-//     backgroundColor: "#61dafb",
-//     color: "#20232a",
-//     textAlign: "center",
-//     fontSize: 30,
-//     fontWeight: "bold",
-//   },
-// });
+
---
+++ firebase/config.js
@@ -0,0 +1,22 @@
+
+import { initializeApp } from 'firebase/app';
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: 'react-native-course-948db',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
