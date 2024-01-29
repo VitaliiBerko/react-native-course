8801c420ec76059c005dbed9f2a40f344d9d0bb4
Vitalii Berko <vitalii.v.berko@gmail.com>
2024-01-29 16:55:41 +0200

    Updated

--- firebase/config.js
+++ firebase/config.js
@@ -1,18 +1,16 @@
-
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: 'react-native-course-948db',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
-  measurementId: '',
+  apiKey: "AIzaSyCS7HO6Uz6PQJfxs3zwTwqdDG4G_qgPevc",
+  authDomain: "react-native-course-948db.firebaseapp.com",
+  projectId: "react-native-course-948db",
+  storageBucket: "react-native-course-948db.appspot.com",
+  messagingSenderId: "541972287173",
+  appId: "1:541972287173:web:b01399e25979cf1917aaac",
+  measurementId: "G-R9KWC8C8NK",
 };
 
 const app = initializeApp(firebaseConfig);
